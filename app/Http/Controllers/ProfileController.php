6e6c5869bd42550b1a5c6f1063107bcca00d972c
theanh204 <20224145@eaut.edu.vn>
2026-04-19 20:26:14 +0700

    theanh204/fix: Sửa lỗi middleware trong ProfileController

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -8,11 +8,6 @@ use Illuminate\Support\Facades\Auth;
 
 class ProfileController extends Controller
 {
-    public function __construct()
-    {
-        $this->middleware('auth');
-    }
-
     public function index()
     {
         $user = Auth::user();
